0d3ab18175314a03a5b422f12834f687eec03067
Eduardo Dias <103222318+eduardoazvdias@users.noreply.github.com>
2023-11-09 20:42:57 -0300

    feature: Kaisen-4 create react app scafolding

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          JuJuTsu Auth
+      </header>
+    </div>
+  );
+}
+
+export default App;
